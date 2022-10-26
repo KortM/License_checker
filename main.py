2b38fa0b1925db8c0d6b0931cf3a02ef9867f4a5
Марченко Игорь Анатольевич <Marchenko-I@gis.lan>
2022-10-26 14:55:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import datetime
+from distutils.command.build import build
+from bs4 import BeautifulSoup
+from tkinter import Tk
+from tkinter.filedialog import askopenfile, askopenfilename
+import re
+import time
+from colorama import init
+from colorama import Fore
+init()
+
+def main():
+    while True:
+        keys = input(Fore.WHITE +
+            'Выберите файл с лицензиями:\nДля продолжения нажмите любую клавишу'
+        )
+        Tk().withdraw()
+        html_license = askopenfilename()
+        #os.system(os.path.realpath(html_license))
+        expired_licese = []
+        try:
+            with open(html_license, 'r') as f:
+                tmp = f.read()
+                soap = BeautifulSoup(tmp, 'html.parser')
+                table = soap.find('table')
+                rows = table.find_all('tr')
+                data = []
+                for row in rows:
+                    cols = row.find_all('td')
+                    cols = [val.text.strip() for val in cols]
+                    data.append(cols)
+                print(data[0])
+                def complete_date(value: str) -> datetime:
+                    match_week_mounth = re.findall(r'[A-Za-z]{3}', value)
+                    match_day_time = re.findall(r'\d{1,2}', value) 
+                    match_year = re.findall(r'\d{4}', value)
+                    #print(match_week_mounth, match_day_time, match_year)
+                    build_date = datetime.datetime(
+                        day=int(match_day_time[0]),
+                        month=int(time.strptime(match_week_mounth[1], '%b').tm_mon),
+                        year=int(match_year[0]),
+                        hour=int(match_day_time[1]),
+                        minute=int(match_day_time[2]),
+                        second=int(match_day_time[3])
+                    )
+                    return build_date
+
+                for line in data:
+                    domain_name = line[1]
+                    match_license_date = re.findall(r' [A-za-z]{3} [A-za-z]{3} {1,2}\d{0,2} \d{1,2}:\d{1,2}:\d{1,2} \d{4}', line[2])
+                    #print(domain_name, match_license_date)
+                    
+                    try:
+                        #print(complete_date(match_license_date[0].strip()))
+                        result = complete_date(match_license_date[1].strip()) -datetime.datetime.now()
+                        if result.days <=30:
+                            print(Fore.RED + 'Срок лицензии истекает: {}\nДаты: {}\nОсталось:{} дней\n'.format(
+                                domain_name, match_license_date, result.days
+                            ))
+                            expired_licese.append('Срок лицензии истекает: {}\nДаты: {}\nОсталось:{} дней\n'.format(
+                                domain_name, match_license_date, result.days
+                            ).encode('utf-8'))
+                    except IndexError as e:
+                        print('Нет лицензии!', e)
+                with open('result.txt', 'wb') as file:
+                    for val in expired_licese:
+                        file.write(val)
+                        file.write('{}\n'.format('-'*20).encode('utf-8'))
+                    file.write('Количество истекающих лицензий: {}'.format(len(expired_licese)).encode('utf-8'))
+            print(Fore.YELLOW + 'Количество истекающих лицензий: {}'.format(Fore.WHITE + str(len(expired_licese))))
+            print(Fore.GREEN + 'Результаты выполнения храняться в файле result.txt рядом с программой\n')            
+        except FileNotFoundError:
+            print('Файл не найден или не существует! Проверьте путь к файлу.')
+
+main()
